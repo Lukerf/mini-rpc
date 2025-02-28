0e69db0204ee3b4a6095312970f0d279f4593d79
yinxiguang <yinxiguang@linklogis.com>
2025-02-28 14:25:35 +0800

    mini-rpc

---
+++ rpc-registry/src/main/java/org/example/ServiceMeta.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+@Data
+public class ServiceMeta {
+
+    private String serviceName;
+
+    private String serviceVersion;
+
+    private String serviceAddr;
+
+    private int servicePort;
+
+}
